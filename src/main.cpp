f9646e38a7ac17325cd587f27923fcd01042c4fe
Light-Lens <QCoreNest@gmail.com>
2023-08-18 23:05:41 +0530

    build the initial entrypoint

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+bool endswith(const string& sentence, const string& what_to_find)
+{
+    return sentence.length() >= what_to_find.length() &&
+        sentence.compare(sentence.length() - what_to_find.length(), what_to_find.length(), what_to_find) == 0;
+}
+
 int main(int argv, char** args)
 {
-    cout << "Hello world!" << endl;
+    string name = args[1];
+    if (argv > 1)
+    {
+        if (endswith(name, ".pastel"))
+            cout << name << endl;
+    }
+
     return 0;
 }
