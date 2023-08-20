e9e73301c89d6dd093dabce45f73b0d5a888fe46
Light-Lens <QCoreNest@gmail.com>
2023-08-20 13:38:43 +0530

    build the tokenizer

--- src/Pastel/lexer.cpp
+++ src/Pastel/lexer.cpp
@@ -1,35 +1,295 @@
 #include "../pastelpch.h"
 #include "../utils/strings.h"
+#include "errors.h"
 #include "lexer.h"
 
 namespace Pastel
 {
+    enum TokenType
+    {
+        EMPTY = 0,
+        UNKNOWN,
+        COMMENT,
+        SYMBOL,
+        STRING,
+        INT,
+        FLOAT,
+        CHAR,
+        KEYWORD,
+        IDENTIFIER,
+        OPERATOR,
+        LPAREN,
+        RPAREN
+    };
+
+    struct lexer::Token
+    {
+        TokenType type;
+        std::string name;
+    };
+
     lexer::lexer(std::fstream& file)
     {
         while (std::getline(file, current_line))
         {
-            std::vector<std::string> a = parser();
-            for (int i = 0; i < a.size(); i++)
-                std::cout << a[i] << std::endl;
+            std::vector<Token> tokens = tokenizer();
+            for (int i = 0; i < tokens.size(); i++)
+                std::cout << tokens[i].name << " (" << tokens[i].type << ")" << std::endl;
 
             ++current_line_no;
         }
     }
 
-    std::vector<std::string> lexer::parser()
+    std::vector<lexer::Token> lexer::tokenizer()
     {
         std::string tok;
-        std::vector<std::string> tokens;
+        std::vector<Token> tokens;
 
         for (int i = 0; i < current_line.size(); i++)
         {
             tok += current_line[i];
 
-            if (tok == "include")
-                tok = "#include";
+            if (utils::is_empty(tok))
+            {
+                tokens.push_back({EMPTY, tok});
+                tok.clear();
+            }
+
+            else if (current_line[i] == '/' && current_line[i+1] == '/')
+            {
+                i++;
+                while (i < current_line.size())
+                {
+                    tok += current_line[i];
+                    i++;
+                }
+
+                i--;
+
+                tokens.push_back({COMMENT, tok});
+                tok.clear();
+                break;
+            }
+
+            else if (is_symbol(tok))
+            {
+                tokens.push_back({SYMBOL, tok});
+                tok.clear();
+            }
+
+            else if (is_operator(current_line[i]))
+            {
+                tokens.push_back({OPERATOR, tok});
+                tok.clear();
+            }
+
+            else if (is_identifier(tok))
+            {
+                i++;
+                while (i < current_line.size() && is_identifier(std::string(1, current_line[i])))
+                {
+                    tok += current_line[i];
+                    i++;
+                }
+
+                i--;
+
+                tokens.push_back({IDENTIFIER, tok});
+                tok.clear();
+            }
+
+            else if (is_float(tok))
+            {
+                i++;
+                while (i < current_line.size() && is_float(std::string(1, current_line[i])))
+                {
+                    tok += current_line[i];
+                    i++;
+                }
+
+                i--;
+
+                if (is_int(tok))
+                    tokens.push_back({INT, tok});
+
+                else
+                    tokens.push_back({FLOAT, tok});
+
+                tok.clear();
+            }
+
+            else if (tok == "'" || tok == "\"")
+            {
+                parse_string(current_line, tok, current_line[i], i);
+
+                if (current_line[i] == '"')
+                    tokens.push_back({STRING, tok});
+
+                else if (current_line[i] == '\'')
+                {
+                    if (tok.size() > 3)
+                        errors::runtime("Multi-character literal at line " + std::to_string(current_line_no));
+
+                    tokens.push_back({CHAR, tok});
+                }
+
+                tok.clear();
+            }
+
+            else
+            {
+                tokens.push_back({UNKNOWN, tok});
+                tok.clear();
+            }
         }
 
-        tokens.push_back(tok);
         return tokens;
     }
+
+    bool lexer::is_identifier(const std::string& str)
+    {
+        if (utils::is_empty(str))
+            return false;
+
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (!isalpha(str[i]) || str[i] == '_')
+                return false;
+        }
+
+        return true;
+    }
+
+    bool lexer::is_int(const std::string& str)
+    {
+        if (utils::is_empty(str))
+            return false;
+
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (!isdigit(str[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    bool lexer::is_float(const std::string& str)
+    {
+        if (utils::is_empty(str))
+            return false;
+
+        int dot_count = 0;
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (isdigit(str[i]))
+                continue;
+
+            else if (str[i] == '.' && dot_count < 2)
+            {
+                dot_count++;
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    bool lexer::is_operator(const char& c)
+    {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' ||
+               c == '=' || c == '<' || c == '>' ||
+               c == '!' || c == '&' || c == '|';
+    }
+
+    bool lexer::is_symbol(const std::string& str)
+    {
+        return str == "::" || str == ":" || str == "." || str == "," || str == ";";
+    }
+
+    void lexer::parse_string(const std::string& current_line, std::string& tok, const char& str_symbol, int& i)
+    {
+        i++;
+        while (i < current_line.size() && current_line[i] != str_symbol)
+        {
+            if (current_line[i] == '\\')
+            {
+                i++;
+                switch (current_line[i])
+                {
+                case '\\':
+                    tok += "\\";
+                    break;
+
+                case '"':
+                    tok += "\"";
+                    break;
+
+                case '\'':
+                    tok += "'";
+                    break;
+
+                case 'n':
+                    tok += "\n";
+                    break;
+
+                case '0':
+                    tok += "\0";
+                    break;
+
+                case 't':
+                    tok += "\t";
+                    break;
+
+                case 'r':
+                    tok += "\r";
+                    break;
+
+                case 'b':
+                    tok += "\b";
+                    break;
+
+                case 'a':
+                    tok += "\a";
+                    break;
+
+                case 'f':
+                    tok += "\f";
+                    break;
+
+                default:
+                    tok += "\\" + std::string(1, current_line[i]);
+                    break;
+                }
+            }
+
+            else
+                tok += current_line[i];
+
+            i++;
+        }
+
+        tok += current_line[i];
+        if (i >= current_line.size())
+            errors::runtime("Unterminated char at line " + std::to_string(current_line_no));
+    }
+
+    // std::vector<std::string> lexer::parser()
+    // {
+    //     std::string tok;
+    //     std::vector<std::string> tokens;
+
+    //     for (int i = 0; i < current_line.size(); i++)
+    //     {
+    //         tok += current_line[i];
+
+    //         if (tok == "include")
+    //             tok = "#include";
+    //     }
+
+    //     tokens.push_back(tok);
+    //     return tokens;
+    // }
 }
--- src/Pastel/lexer.h
+++ src/Pastel/lexer.h
@@ -4,10 +4,8 @@ namespace Pastel
 {
     class lexer
     {
-    public:
-        struct Token;
-
     private:
+        struct Token;
         std::string current_line;
         int current_line_no = 1;
 
@@ -15,6 +13,12 @@ namespace Pastel
         lexer(std::fstream& file);
 
     private:
-        std::vector<std::string> parser();
+        std::vector<lexer::Token> tokenizer();
+        bool is_identifier(const std::string& str);
+        bool is_int(const std::string& str);
+        bool is_float(const std::string& str);
+        bool is_operator(const char& c);
+        bool is_symbol(const std::string& str);
+        void parse_string(const std::string& current_line, std::string& tok, const char& str_symbol, int& i);
     };
 }
