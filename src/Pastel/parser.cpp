647f2fe9491b7d260a0be531fa9a510ec4701597
Light-Lens <QCoreNest@gmail.com>
2023-08-21 10:59:00 +0530

    the 1st iter of parser translator

--- src/Pastel/parser.cpp
+++ src/Pastel/parser.cpp
@@ -6,27 +6,54 @@ namespace Pastel
 {
     void parser::translator(const std::vector<token>& tokens)
     {
-        std::string tok;
         std::vector<std::string> translation;
+        if (tokens[0].type == COMMENT)
+            translation.push_back(tokens[0].name);
 
-        for (int i = 0; i < tokens.size(); i++)
+        else if (tokens[0].type == KEYWORD && tokens[1].type == STRING)
         {
-            tok += tokens[i].name;
-
-            if (tok == "include")
-                tok = "#include";
-
-            else if (tok == "let")
-                tok = "auto";
-
-            else if (tok == "string")
-                tok = "std::string";
-
-            else if (tok == "fun")
-                tok = "void";
+            if (tokens[0].name == "include")
+            {
+                translation.push_back("#include");
+                translation.push_back(tokens[1].name);
+            }
         }
 
-        translation.push_back(tok);
+        else if (tokens[0].type == SYMBOL && tokens[1].type == KEYWORD && tokens[2].type == STRING)
+        {
+            if (tokens[0].name == "#" && tokens[1].name == "include")
+            {
+                translation.push_back("#include");
+                translation.push_back(tokens[2].name);
+            }
+        }
+
+        else if (tokens[0].type == KEYWORD && tokens[1].type == OPERATOR && tokens[2].type == IDENTIFIER && tokens[3].type == OPERATOR)
+        {
+            if (tokens[0].name == "include" && tokens[1].name == "<" && tokens[3].name == ">")
+            {
+                translation.push_back("#include");
+                translation.push_back(tokens[2].name + tokens[3].name + tokens[4].name);
+            }
+        }
+
+        else if (tokens[0].type == SYMBOL && tokens[1].type == KEYWORD && tokens[2].type == OPERATOR && tokens[3].type == IDENTIFIER && tokens[4].type == SYMBOL &&  tokens[5].type == IDENTIFIER && tokens[6].type == OPERATOR)
+        {
+            if (tokens[0].name == "#" && tokens[1].name == "include" && tokens[2].name == "<" && tokens[6].name == ">")
+            {
+                translation.push_back("#include");
+                translation.push_back(tokens[2].name + tokens[3].name + tokens[4].name + tokens[5].name + tokens[6].name);
+            }
+        }
+
+        else if (tokens[0].type == SYMBOL && tokens[1].type == KEYWORD && tokens[2].type == OPERATOR && tokens[3].type == IDENTIFIER && tokens[4].type == OPERATOR)
+        {
+            if (tokens[0].name == "#" && tokens[1].name == "include" && tokens[2].name == "<" && tokens[4].name == ">")
+            {
+                translation.push_back("#include");
+                translation.push_back(tokens[2].name + tokens[3].name + tokens[4].name);
+            }
+        }
 
         for (int i = 0; i < translation.size(); i++)
             std::cout << translation[i] << std::endl;
