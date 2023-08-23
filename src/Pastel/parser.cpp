948af85c1fdac4d0a5a1b203c594fc8c8e1d4854
Light-Lens <QCoreNest@gmail.com>
2023-08-23 23:57:24 +0530

    rough update of better include char handling

--- src/Pastel/parser.cpp
+++ src/Pastel/parser.cpp
@@ -48,37 +48,52 @@ namespace Pastel
                     handle_includes(tokens, i);
             }
         }
-
-        // for (int i = 0; i < tokens.size(); i++)
-        // {
-        //     const token_type type = tokens[i].type;
-        //     const std::string name = tokens[i].name;
-
-        //     if (type == COMMENT)
-        //     {
-        //         translation.push_back({name});
-        //         break;
-        //     }
-
-        //     else if (type == KEYWORD)
-        //     {
-        //         handle_includes(tokens, i);
-        //     }
-        // }
     }
 
     // handle includes of external lib.
     void parser::handle_includes(const std::vector<token>& tok, const int& start_index)
     {
-        if (!contains_keywords(tok, include_keywords))
-            return;
-
         std::string include_path;
         for (int i = start_index+1; i < tok.size(); i++)
         {
             if (tok[i].type == STRING)
             {
                 include_path += tok[i].name;
+                break;
+            }
+
+            else if (tok[i].type == OPERATOR && tok[i].name == "<")
+            {
+                include_path += tok[i].name;
+                i++;
+                if (i >= tok.size())
+                    errors::runtime("unexpected end of tokens after '<'", current_line, current_line_no);
+
+
+
+                bool has_closing_char_encountered = false;
+                while (i < tok.size())
+                {
+                    if (tok[i].name == "<" || has_closing_char_encountered)
+                        errors::fatal("invalid argument", current_line, current_line_no);
+
+                    // if (tok[i].name == "<")
+                    //     errors::fatal("invalid argument", current_line, current_line_no);
+
+                    if (tok[i].name == ">" && !has_closing_char_encountered)
+                        has_closing_char_encountered = true;
+
+                    include_path += tok[i].name;
+                    i++;
+                }
+
+                include_path += tok[i].name;
+
+
+
+                if (i >= tok.size() || tok[i].name != ">")
+                    errors::runtime("unexpected end of tokens, missing closing '>'", current_line, current_line_no);
+
                 break;
             }
 
